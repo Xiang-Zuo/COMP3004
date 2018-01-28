8ef8fa6770aca6154f8b8e2d79c43a496aa47d0e
Sirui <35638204+SiruiZeng@users.noreply.github.com>
2018-01-27 23:10:29 -0500

    Revert "rank change version 1"
    This reverts commit ad4fc3a58e95020aca45f6c3615f4b0c5d352cac.

--- TABLE OF REQUEST/Assets/scripts/GameRounds.cs
+++ TABLE OF REQUEST/Assets/scripts/GameRounds.cs
@@ -1,6 +1,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -161,6 +162,90 @@ public class GameRounds : MonoBehaviour {
 	}
 }
 >>>>>>> ad4fc3a58e95020aca45f6c3615f4b0c5d352cac
+=======
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameRounds : MonoBehaviour {
+    bool quit = false;
+	int stat;
+    public GameObject[] Adventure= {};
+   
+
+	/**
+	 * stat:
+	 * 0 for Initial page
+	 * 1 for draw a card from story deck
+	 * 2 for Quest
+	 * 3 for Tournament
+	 * 4 for Event
+	 * 5 for process Q,T,E
+	 * 6 for player's change after a turn 
+	 * 7 for check win, return 1 if not
+	**/
+
+	// Use this for initialization
+	void Start () {
+        stat = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            playing(stat);
+    }
+
+	void playing(int stat)
+	{
+        if (stat == 0)
+        {
+            print("0");
+            stat = 7;
+            playing(stat);
+        }
+            if (stat == 1)
+            {
+            print("1");
+            stat = 2;
+            playing(stat);
+            }
+
+            if (stat == 2)
+            {
+            print("2");
+            stat = 7;
+            playing(stat);
+            }
+
+            if (stat == 3)
+            {
+
+            }
+
+            if (stat == 4)
+            {
+
+            }
+
+            if (stat == 5)
+            {
+
+            }
+
+            if (stat == 6)
+            {
+
+            }
+
+            if (stat == 7)
+            {
+            SceneManager.LoadScene(0);
+            }
+        
+	}
+}
+>>>>>>> parent of ad4fc3a... rank change version 1
 =======
 ﻿using System.Collections;
 using System.Collections.Generic;
