60e2c6d21611893d85d85de76444fdea3ad30a9b
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2018-03-02 13:47:19 -0500

    submit

---
+++ QoRT/Library/Collab/Download/Assets/scripts/Card/AmourCard.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmourCard : Card
+{
+    private int atk;
+
+    public AmourCard(string aName, Kind aKind, int anAtk)
+    {
+        this.setName(aName);
+        this.setKind(aKind);
+        this.atk = anAtk;
+    }
+    public int getAtk()
+    {
+        return this.atk;
+    }
+
+}
---
+++ QoRT/Library/Collab/Download/Assets/scripts/Card/TournamentCard.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TournamentCard : Card {
+    private int bounsNum;
+
+    public TournamentCard(string aName, Kind aKind, int aBounsNum)
+    {
+        this.setName(aName);
+        this.setKind(aKind);
+        this.bounsNum = aBounsNum;
+    }
+    public int getBounsNum()
+    {
+        return this.bounsNum;
+    }
+}
